c5c10a618f1c21ddfc92005e564b1c99d1021019
Dasha Burgos <dasha.lewis@gmail.com>
2023-05-03 21:45:14 -0400

    completed lab 11 features

---
+++ src/main/java/com/dasha/songr/d17/Album.java
@@ -0,0 +1,68 @@
+package com.dasha.songr.d17;
+
+public class Album {
+    private String title;
+    private String artist;
+    private int songCount;
+    private int songLength;
+    private String imgUrl;
+
+    public Album(String title, String artist, int songCount, int songLength, String imgUrl) {
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.songLength = songLength;
+        this.imgUrl = imgUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public int getSongLength() {
+        return songLength;
+    }
+
+    public void setSongLength(int songLength) {
+        this.songLength = songLength;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" +
+                "title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                ", songCount=" + songCount +
+                ", songLength=" + songLength +
+                ", imgUrl='" + imgUrl + '\'' +
+                '}';
+    }
+}
